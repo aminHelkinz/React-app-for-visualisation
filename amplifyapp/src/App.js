e5dd83712226cf93bd13105a07ad4dfe3c607b04
lydialebdiri <lydialebdiri2021@gmail.com>
2023-06-19 14:33:21 +0200

    4 eme commit de amplifyapp

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -1,25 +1,45 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import '@aws-amplify/ui-react/styles.css';
+import { withAuthenticator, Button, Heading, Image, View, Card } from '@aws-amplify/ui-react';
+import { Auth } from 'aws-amplify';
+import { useUploadCsvToS3 } from './S3Upload';
 
-function App() {
+function App({ signOut }) {
+  const [username, setUsername] = useState('');
+  const [csvFile, setCsvFile] = useState(null);
+  const [uploadDate, setUploadDate] = useState(null);
+
+  useEffect(() => {
+    Auth.currentAuthenticatedUser()
+      .then((user) => {
+        setUsername(user.username);
+      })
+      .catch((error) => {
+        console.log('Erreur lors de la récupération de l\'utilisateur authentifié :', error);
+      });
+  }, []);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setCsvFile(file);
+      setUploadDate(file.lastModified);
+    }
+  };
+
+  useUploadCsvToS3(username, csvFile, uploadDate);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <input type="file" onChange={handleFileChange} />
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
