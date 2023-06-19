fe0b6b05ae74e227ff4d57f1ec5c0b6f7849a804
lydialebdiri <lydialebdiri2021@gmail.com>
2023-06-19 14:53:04 +0200

    5 eme commit de amplifyapp

--- amplifyapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplifyapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,16 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyappb0714d68b0714d68": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "AdminGroupRole": "string"
+    }
+  }
+}
