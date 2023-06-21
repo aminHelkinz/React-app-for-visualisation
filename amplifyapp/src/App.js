13167bc2a2d099296a8ec231ce8cd42c7267d86b
lydialebdiri <lydialebdiri2021@gmail.com>
2023-06-21 16:16:29 +0200

    34 eme commit de amplifyapp

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -1,8 +1,9 @@
-import "@aws-amplify/ui-react/styles.css";
+import React, { useEffect, useRef, useState } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { withAuthenticator, Button, Heading, Image, View, Card } from "@aws-amplify/ui-react";
-import { useEffect, useRef, useState } from "react";
 import { Amplify, Storage } from "aws-amplify";
 import { Auth } from "@aws-amplify/auth";
+import VisualizationPage from "./VisualizationPage";
 
 function App({ signOut }) {
   const ref = useRef(null);
@@ -40,30 +41,30 @@ function App({ signOut }) {
 
   useEffect(() => {
     loadFiles();
-  }, []);
+  }, [files]); // Refresh the file list when `files` state changes
 
   const handleFileLoad = async () => {
     const file = ref.current.files[0];
     const currentDate = new Date();
     const timestamp = Math.floor(currentDate.getTime() / 1000);
-  
+
     if (selectedFile) {
       console.log("A file is already selected. Please upload one file at a time.");
       return;
     }
-        if (!file || file.size === 0) {
-      console.log("Please select a non-empty file. ", fileName);
+    if (!file || file.size === 0) {
+      console.log("Please select a non-empty file.");
       return;
     }
-  
+
     try {
       const user = await Auth.currentAuthenticatedUser();
       const userEmail = user.attributes.email;
       const authenticatedUser = user.username;
       const fileName = `${authenticatedUser}-${timestamp}#${userEmail}.csv`;
-  
+
       setSelectedFile(file);
-  
+
       Storage.put(fileName, file, {
         progressCallback: (progress) => {
           setProgress(Math.round((progress.loaded / progress.total) * 100) + "%");
@@ -72,14 +73,13 @@ function App({ signOut }) {
       })
         .then(resp => {
           console.log(resp);
-          loadFiles();
           setSelectedFile(null);
+          loadFiles(); // Refresh the file list after successful upload
         }).catch(err => { console.log(err); setSelectedFile(null); });
     } catch (error) {
       console.log("Error getting authenticated user:", error);
     }
   }
-  
 
   const handleShow = (file) => {
     Storage.get(file).then(resp => {
@@ -96,32 +96,39 @@ function App({ signOut }) {
   }
 
   return (
-    <View className="App">
-      <Card>
-        <Heading level={1}> Welcome dear customer!</Heading>
-      </Card>
-      <div style={{ display: "flex", justifyContent: "flex-end", marginBottom: "1rem" }}>
-        <Button onClick={signOut}>Sign Out</Button>
-      </div>
-      <input ref={ref} type="file" accept=".csv" onChange={handleFileLoad} />
-      {progress}
-      <table>
-        <tbody>
-          {Array.isArray(files) &&
-            files.map((file, i) => (
-              <tr key={file.key}>
-                <td>{i}</td>
-                <td>{file.key}</td>
-                <td>
-                  <Button onClick={() => handleShow(file.key)}>Show</Button>
-                  <Button onClick={() => handleDelete(file.key)}>Delete</Button>
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
-      {image && <Image src={image} alt="File" />}
-    </View>
+    <Router>
+      <View className="App">
+        <Card>
+          <Heading level={1}>Welcome dear customer!</Heading>
+        </Card>
+        <div style={{ display: "flex", justifyContent: "flex-end", marginBottom: "1rem" }}>
+          <Button onClick={signOut}>Sign Out</Button>
+        </div>
+        <input ref={ref} type="file" accept=".csv" onChange={handleFileLoad} />
+        {progress}
+        <table>
+          <tbody>
+            {Array.isArray(files) &&
+              files.map((file, i) => (
+                <tr key={file.key}>
+                  <td>{i}</td>
+                  <td>{file.key}</td>
+                  <td>
+                    <Button onClick={() => handleShow(file.key)}>Show</Button>
+                    <Button onClick={() => handleDelete(file.key)}>Delete</Button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+        {image && <Image src={image} alt="File" />}
+        <Link to="/visualization">Go to Visualization Page</Link>
+      </View>
+
+      <Switch>
+        <Route path="/visualization" component={VisualizationPage} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ amplifyapp/src/VisualizationPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function VisualizationPage() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <h2>Please wait for 1 minute to see your visualizations...</h2>
+    </div>
+  );
+}
+
+export default VisualizationPage;
