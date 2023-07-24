8e4d9d2b4d316cd3a39107ea4808c0cbd19ed7f3
lydialebdiri <lydialebdiri2021@gmail.com>
2023-07-24 10:41:25 +0200

    code using two bucket s3 one for uploading data and second for geting data

---
+++ lydia-project-main/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "reactamplified5592668255926682": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "Csvfilesusers": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
